0f5de823c448931c3a8fe468fc9e5511a3fca7fa
dhruvrrp <dkaushal@ucsd.edu>
2016-04-21 18:16:25 -0500

    Update robot to match the provided code

--- scripts/robot.py
+++ scripts/robot.py
@@ -3,31 +3,69 @@
 import rospy
 from std_msgs.msg import Bool
 from cse_190_assi_1.msg import temperatureMessage
+from cse_190_assi_1.msg import RobotProbabilities
 
-def callback(data):
-    print "CALLBACK"
-    print data
+class Robot():
+    def __init__(self):
 
-def robot():
-    pub = rospy.Publisher('/temp_sensor/activation', Bool, queue_size=10)
-    rospy.Subscriber("/temp_sensor/data", temperatureMessage, callback)
+        self.config = read_config()
+        rospy.init_node("robot")
+        self.texture_requester = rospy.ServiceProxy(
+                "requestTexture",
+                requestTexture
+        )
+        self.temperature_subscriber = rospy.Subscriber(
+                "/temp_sensor/data",
+                Bool,
+                self.handle_temperature
+        )
+        self.temperature_activator = rospy.Publisher(
+                "/temp_sensor/activation",
+                temperatureMessage,
+                queue_size = 10
+        )
+        self.temperature_data = rospy.Publisher(
+                "/results/temperature_data",
+                temperatureMessage,
+                queue_size = 10
+        )
+        self.texture_data = rospy.Publisher(
+                "/results/texture_data",
+                String,
+                queue_size = 10
+        )
+        self.probabilities = rospy.Publisher(
+                "/results/probabilities",
+                RobotProbabilities,
+                queue_size = 10
+        )
 
-    rospy.init_node('robot', anonymous=True)
-    rate = rospy.Rate(10)
-    ar = True
-    while not rospy.is_shutdown():
-       # rospy.loginfo(ar)
-        if ar: 
-            pub.publish(ar)
-            print "robot"
-        rospy.Subscriber("/temp_sensor/data", temperatureMessage, callback)
-        rate.sleep()
-    rospy.spin()
-   
+        self.move_list = self.config["move_list"]
+        self.temp_message = temperatureMessage()
+        self.sensor_on = False
+        self.got_callback = True
+        random.seed(self.config['seed'])
+        self.rate = rospy.Rate(1)
+        self.sensor_loop()
+
+
+    def sensor_loop(self):
+
+        while not rospy.is_shutdown():
+            if self.sensor_on and self.got_callback:
+                self.temperature_activator.publish(self.sensor_on)
+                self.rate.sleep()
+
+
+    def handle_temperature(self, message):
+
+        self.got_callback = False
+        text_response = texture_requester('temp')
+        texture = text_response.data
         
-def lir():
 
-    rospy.Subscriber("/temp_sensor/data", temperatureMessage, callback)
+
+
 
 
 if __name__ == '__main__':
